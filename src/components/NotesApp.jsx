33c2bba3704c3be4c7f401b98141610b8faf0a38
Farid Arya Nugraha <farid.a.nugraha@sat.co.id>
2023-12-06 14:51:45 +0700

    success add note

--- src/components/NotesApp.jsx
+++ src/components/NotesApp.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NoteList from "./NotesList"
+import NoteInput from "./NoteInput"
 import { getNotes } from "../utils/notes";
 import { Container } from "react-bootstrap";
 
@@ -13,6 +14,7 @@ class NotesApp extends React.Component{
         }
 
         this.onDeleteNoteHandler = this.onDeleteNoteHandler.bind(this)
+        this.onAddNoteHandler = this.onAddNoteHandler.bind(this)
     }
 
     onDeleteNoteHandler(id){
@@ -20,11 +22,30 @@ class NotesApp extends React.Component{
         this.setState({notes})
     }
 
+    onAddNoteHandler({title, body}){
+        const currentDate = new Date()
+        this.setState((prevState) => {
+            return {
+                notes: [
+                    ...prevState.notes,
+                    {
+                        id: +new Date(),
+                        title: title,
+                        body: body,
+                        archived: false,
+                        createdAt: currentDate.toJSON()
+                    }
+                ]
+            }
+        })
+    }
+
     render(){
         return (
             <Container className="notes-app">
                 <h1>Notes App</h1>
                 <p>make your notes, elevate your works</p>
+                <NoteInput addNote={this.onAddNoteHandler}/>
 
                 <h2>Recent Notes</h2>
                 <NoteList notes={this.state.notes} isArchived={false} onDelete={this.onDeleteNoteHandler}/>
