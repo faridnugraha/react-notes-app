66203ccc46aefbb3105146dd390fdab612679144
Farid Arya <farid.a.nugraha@sat.co.id>
2024-01-25 21:19:23 +0700

    done update api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NoteList from "../components/NotesList"
 import NoteSearch from "../components/NoteSearch"
@@ -7,84 +7,136 @@ import { Container, Tab, Tabs, Row, Col } from "react-bootstrap";
 import { PropTypes } from 'prop-types'
 import { useSearchParams } from "react-router-dom";
 
-function HomePageWrapper({notes, deleteHandler, archivedHandler}){
+function HomePage({ notes, deleteHandler, archivedHandler }){
     const [searchParams, setSearchParams] = useSearchParams()
+    const [keyword, setKeyword] = useState(() => {
+        return searchParams.get('keyword') || ''
+    })
 
-    const keyword = searchParams.get('keyword')
-
-    function changeSearchParams(keyword){
+    function onKeywordChangeHandler(keyword){
+        setKeyword(keyword)
         setSearchParams({keyword})
     }
 
-    return <HomePage notes={notes} deleteHandler={deleteHandler} archivedHandler={archivedHandler} defaultKeyword={keyword} changeSearchParams={changeSearchParams}/>
-}
-
-class HomePage extends React.Component{
-    constructor(props){
-        super(props)
-
-        this.state = {
-            keyword: props.defaultKeyword || ''
-        }
-
-        this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this)
-    }
-
-    onKeywordChangeHandler(keyword){
-        this.setState(() => {
-            return {keyword}
-        })
-
-        this.props.changeSearchParams(keyword)
-    }
-
-    render(){
-        const notes = this.props.notes.filter((note) => {
-            return note.title.toLowerCase().includes(
-                this.state.keyword.toLowerCase()
-            )
-        })
-
-        return (
-            <>
-                <header>
-                    <Container>
-                        <Row>
-                            <Col md={6}>
-                                <h1 id="app-title">Notes App</h1>
-                                <NoteHeader/>
-                                <NoteSearch keyword={this.state.keyword} keywordHandler={this.onKeywordChangeHandler}/>
-                            </Col>
-                        </Row>
-                    </Container>
-                </header>
-                <Container className="notes-app">
-                    <Tabs variant="pills" defaultActiveKey="recent" id="category-tab" className="mt-4 mb-3 mx-auto tabs-custom" fill>
-                        <Tab eventKey="recent" title="Recent">
-                            <NoteList notes={notes} isArchived={false} onDelete={this.props.deleteHandler} onArchiveToggle={this.props.archivedHandler}/>
-                        </Tab>
-                        <Tab eventKey="archive" title="Archive">
-                            <NoteList notes={notes} isArchived={true} onDelete={this.props.deleteHandler} onArchiveToggle={this.props.archivedHandler}/>
-                        </Tab>
-                    </Tabs>
-                </Container>
-            </>
+    const filteredNotes = notes.filter((note) => {
+        return note.title.toLowerCase().includes(
+            keyword.toLowerCase()
         )
-    }
-}
+    })
 
-HomePageWrapper.propTypes = {
-    notes: PropTypes.arrayOf(PropTypes.object).isRequired,
-    deleteHandler : PropTypes.func.isRequired,
-    archivedHandler : PropTypes.func.isRequired
+    return (
+        <>
+            <header>
+                <Container>
+                    <Row>
+                        <Col md={6}>
+                            <h1 id="app-title">Notes App</h1>
+                            <NoteHeader/>
+                            <NoteSearch keyword={keyword} keywordHandler={onKeywordChangeHandler}/>
+                        </Col>
+                    </Row>
+                </Container>
+            </header>
+            <Container className="notes-app">
+                <Tabs variant="pills" defaultActiveKey="recent" id="category-tab" className="mt-4 mb-3 mx-auto tabs-custom" fill>
+                    <Tab eventKey="recent" title="Recent">
+                        <NoteList notes={filteredNotes} isArchived={false} onDelete={deleteHandler} onArchiveToggle={archivedHandler}/>
+                    </Tab>
+                    <Tab eventKey="archive" title="Archive">
+                        <NoteList notes={filteredNotes} isArchived={true} onDelete={deleteHandler} onArchiveToggle={archivedHandler}/>
+                    </Tab>
+                </Tabs>
+            </Container>
+        </>
+    )
 }
 
 HomePage.propTypes = {
     notes: PropTypes.arrayOf(PropTypes.object).isRequired,
     deleteHandler : PropTypes.func.isRequired,
     archivedHandler : PropTypes.func.isRequired,
-    defaultKeyword: PropTypes.string,
-    changeSearchParams: PropTypes.func.isRequired
 }
 
-export default HomePageWrapper;
+export default HomePage;
+
+// function HomePageWrapper({notes, deleteHandler, archivedHandler}){
+//     const [searchParams, setSearchParams] = useSearchParams()
+
+//     const keyword = searchParams.get('keyword')
+
+//     function changeSearchParams(keyword){
+//         setSearchParams({keyword})
+//     }
+
+//     return <HomePage notes={notes} deleteHandler={deleteHandler} archivedHandler={archivedHandler} defaultKeyword={keyword} changeSearchParams={changeSearchParams}/>
+// }
+
+// class HomePage extends React.Component{
+//     constructor(props){
+//         super(props)
+
+//         this.state = {
+//             keyword: props.defaultKeyword || ''
+//         }
+
+//         this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this)
+//     }
+
+//     onKeywordChangeHandler(keyword){
+//         this.setState(() => {
+//             return {keyword}
+//         })
+
+//         this.props.changeSearchParams(keyword)
+//     }
+
+//     render(){
+//         const notes = this.props.notes.filter((note) => {
+//             return note.title.toLowerCase().includes(
+//                 this.state.keyword.toLowerCase()
+//             )
+//         })
+
+//         return (
+//             <>
+//                 <header>
+//                     <Container>
+//                         <Row>
+//                             <Col md={6}>
+//                                 <h1 id="app-title">Notes App</h1>
+//                                 <NoteHeader/>
+//                                 <NoteSearch keyword={this.state.keyword} keywordHandler={this.onKeywordChangeHandler}/>
+//                             </Col>
+//                         </Row>
+//                     </Container>
+//                 </header>
+//                 <Container className="notes-app">
+//                     <Tabs variant="pills" defaultActiveKey="recent" id="category-tab" className="mt-4 mb-3 mx-auto tabs-custom" fill>
+//                         <Tab eventKey="recent" title="Recent">
+//                             <NoteList notes={notes} isArchived={false} onDelete={this.props.deleteHandler} onArchiveToggle={this.props.archivedHandler}/>
+//                         </Tab>
+//                         <Tab eventKey="archive" title="Archive">
+//                             <NoteList notes={notes} isArchived={true} onDelete={this.props.deleteHandler} onArchiveToggle={this.props.archivedHandler}/>
+//                         </Tab>
+//                     </Tabs>
+//                 </Container>
+//             </>
+//         )
+//     }
+// }
+
+// HomePageWrapper.propTypes = {
+//     notes: PropTypes.arrayOf(PropTypes.object).isRequired,
+//     deleteHandler : PropTypes.func.isRequired,
+//     archivedHandler : PropTypes.func.isRequired
+// }
+
+// HomePage.propTypes = {
+//     notes: PropTypes.arrayOf(PropTypes.object).isRequired,
+//     deleteHandler : PropTypes.func.isRequired,
+//     archivedHandler : PropTypes.func.isRequired,
+//     defaultKeyword: PropTypes.string,
+//     changeSearchParams: PropTypes.func.isRequired
+// }
+
+// export default HomePageWrapper;
--- src/pages/NoteDetailPage.jsx
+++ src/pages/NoteDetailPage.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Container, Row, Col, Card} from "react-bootstrap";
 import NoteManageButton from "../components/NoteManageButton";
 import { getNote } from "../utils/local-data";
@@ -7,74 +6,126 @@ import { PropTypes } from "prop-types";
 import BackHomeButton from "../components/BackHomeButton";
 import { useNavigate } from 'react-router-dom'
 import NotFoundPage from "./NotFoundPage";
+import { useState } from "react";
 
-
-function NoteDetailPageWrapper({archivedHandler, deleteHandler}){
+function NoteDetailPage({ deleteHandler, archivedHandler}){
     const { id } = useParams()
     const navigate = useNavigate()
-    return <NoteDetailPage navigate={navigate} id={id} archivedHandler={archivedHandler} deleteHandler={deleteHandler}/>
-}
+    const [note, setNote] = useState(() => {
+        return getNote(id)
+    })
 
-class NoteDetailPage extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {  
-            note : getNote(this.props.id)
-        }
-
-        this.onArchiveHandler = this.onArchiveHandler.bind(this)
-        this.onDeleteHandler = this.onDeleteHandler.bind(this)
+    function onArchivedHandler(){
+        archivedHandler(note.id, note.archived)
+        setNote(getNote(id))
     }
 
-    onArchiveHandler(){
-        this.props.archivedHandler(this.state.note.id, this.state.note.archived)
-        this.setState({note: getNote(this.props.id)})
+    function onDeleteHandler(){
+        deleteHandler(note.id)
+        navigate('/')
+    }
+
+    if(note === undefined){
+        return <NotFoundPage />
     }
     
-    onDeleteHandler(){
-        this.props.deleteHandler(this.state.note.id)
-        this.props.navigate('/')
-    }
-
-    render(){
-        if (this.state.note === undefined) {
-           return <NotFoundPage/>
-        }
-        return(
-            <Container>
-                <Row className="justify-content-center mt-5">
-                    <Col xs md={6}>
-                        <BackHomeButton/>
-                        <Card>
-                            <Card.Header>
-                                <Card.Title>
-                                {this.state.note.title}
-                                </Card.Title>
-                            </Card.Header>
-                            <Card.Body>
-                                <p>{this.state.note.body}</p>
-                            </Card.Body>
-                            <Card.Footer className="d-flex justify-content-end">
-                                <NoteManageButton id={this.state.note.id} archived={this.state.note.archived} onArchiveToggle={this.onArchiveHandler} deleteButtonHandler={this.onDeleteHandler}/>
-                            </Card.Footer>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
-}
-
-NoteDetailPageWrapper.propTypes = {
-    archivedHandler: PropTypes.func.isRequired,
-    deleteHandler: PropTypes.func.isRequired
+    return (
+        <Container>
+            <Row className="justify-content-center mt-5">
+                <Col xs md={6}>
+                    <BackHomeButton/>
+                    <Card>
+                        <Card.Header>
+                            <Card.Title>
+                            {note.title}
+                            </Card.Title>
+                        </Card.Header>
+                        <Card.Body>
+                            <p>{note.body}</p>
+                        </Card.Body>
+                        <Card.Footer className="d-flex justify-content-end">
+                            <NoteManageButton id={note.id} archived={note.archived} onArchiveToggle={onArchivedHandler} deleteButtonHandler={onDeleteHandler}/>
+                        </Card.Footer>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 
 NoteDetailPage.propTypes = {
-    id: PropTypes.string.isRequired,
-    navigate: PropTypes.func.isRequired,
     archivedHandler: PropTypes.func.isRequired,
     deleteHandler: PropTypes.func.isRequired
 }
 
-export default NoteDetailPageWrapper
+export default NoteDetailPage
+
+// function NoteDetailPageWrapper({archivedHandler, deleteHandler}){
+//     const { id } = useParams()
+//     const navigate = useNavigate()
+//     return <NoteDetailPage navigate={navigate} id={id} archivedHandler={archivedHandler} deleteHandler={deleteHandler}/>
+// }
+
+// class NoteDetailPage extends React.Component{
+//     constructor(props){
+//         super(props)
+//         this.state = {  
+//             note : getNote(this.props.id)
+//         }
+
+//         this.onArchiveHandler = this.onArchiveHandler.bind(this)
+//         this.onDeleteHandler = this.onDeleteHandler.bind(this)
+//     }
+
+//     onArchiveHandler(){
+//         this.props.archivedHandler(this.state.note.id, this.state.note.archived)
+//         this.setState({note: getNote(this.props.id)})
+//     }
+    
+//     onDeleteHandler(){
+//         this.props.deleteHandler(this.state.note.id)
+//         this.props.navigate('/')
+//     }
+
+//     render(){
+//         if (this.state.note === undefined) {
+//            return <NotFoundPage/>
+//         }
+//         return(
+//             <Container>
+//                 <Row className="justify-content-center mt-5">
+//                     <Col xs md={6}>
+//                         <BackHomeButton/>
+//                         <Card>
+//                             <Card.Header>
+//                                 <Card.Title>
+//                                 {this.state.note.title}
+//                                 </Card.Title>
+//                             </Card.Header>
+//                             <Card.Body>
+//                                 <p>{this.state.note.body}</p>
+//                             </Card.Body>
+//                             <Card.Footer className="d-flex justify-content-end">
+//                                 <NoteManageButton id={this.state.note.id} archived={this.state.note.archived} onArchiveToggle={this.onArchiveHandler} deleteButtonHandler={this.onDeleteHandler}/>
+//                             </Card.Footer>
+//                         </Card>
+//                     </Col>
+//                 </Row>
+//             </Container>
+//         )
+//     }
+// }
+
+// NoteDetailPageWrapper.propTypes = {
+//     archivedHandler: PropTypes.func.isRequired,
+//     deleteHandler: PropTypes.func.isRequired
+// }
+
+// NoteDetailPage.propTypes = {
+//     id: PropTypes.string.isRequired,
+//     navigate: PropTypes.func.isRequired,
+//     archivedHandler: PropTypes.func.isRequired,
+//     deleteHandler: PropTypes.func.isRequired
+// }
+
+// export default NoteDetailPageWrapper
